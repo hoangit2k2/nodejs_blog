9504155a1e29d211436095810cc038f8dd7847c8
hoangit2k2 <nguyenhoang2002it@gmail.com>
2022-08-29 15:36:16 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express')
-const morgan = require('morgan')
+const mgorgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
