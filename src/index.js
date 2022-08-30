3361b4753aac8215dd04997a6be7a8083224d0ea
hoangit2k2 <nguyenhoang2002it@gmail.com>
2022-08-30 18:40:39 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express')
-const mgorgan = require('morgan')
+const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(morgan('combined'))
 //teamlate enginer
 app.engine('handlebars', handlebars.engine());
 app.set('view engine','handlebars');
 app.set('views', path.join(__dirname,'resoruces/views'))
+
 app.get('/', (req, res) => {
   res.render('home');
 })
