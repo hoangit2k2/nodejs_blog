1de1339c5e2ea4f9b7c751e6b02a341802847ab6
hoangit2k2 <109071123+MichaelTranBED@users.noreply.github.com>
2022-09-09 09:08:32 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -6,14 +6,24 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname,'public')));
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 //teamlate enginer
 app.engine('handlebars', handlebars.engine());
 app.set('view engine','handlebars');
 app.set('views', path.join(__dirname,'resoruces/views'))
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.render('home');
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.get('/search',(req,res) => {
+  console.log(req.query.q);
+  console.log(req.query.auth);
+  res.render('search')
 })
 
 app.listen(port, () => {
